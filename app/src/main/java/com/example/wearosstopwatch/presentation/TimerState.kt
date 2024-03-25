fbee7b95ec74515a2f6fdc251d051cf247c8d149
Christopher Abbod <chris.abbod@august.com>
2024-03-25 15:48:14 -0400

    Add TimerState Enum Class

---
+++ app/src/main/java/com/example/wearosstopwatch/presentation/TimerState.kt
@@ -0,0 +1,9 @@
+package com.example.wearosstopwatch.presentation
+
+enum class TimerState {
+    // Differentiates the different states the Stopwatch can be in
+    RUNNING,
+    PAUSED,
+    RESET // Initial state
+
+}
