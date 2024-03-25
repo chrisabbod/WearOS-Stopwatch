235b018da54eea6a56f2adec7bb0a20601c1fd50
Christopher Abbod <chris.abbod@august.com>
2024-03-25 11:54:26 -0400

    Add initial project files

---
+++ app/src/main/java/com/example/wearosstopwatch/presentation/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.wearosstopwatch.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.TimeText
+import com.example.wearosstopwatch.R
+import com.example.wearosstopwatch.presentation.theme.WearOsStopWatchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/wearosstopwatch/presentation/StopWatchViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.wearosstopwatch.presentation
+
+import androidx.lifecycle.ViewModel
+
+class StopWatchViewModel: ViewModel() {
+
+    
+
+}
